714037fc2baa7ba27cc7281901a9fa458f1878d8
shisaq <shisaq@gmail.com>
2018-06-18 15:05:39 -0400

    feat: add carousel to demo page

---
+++ src/components/Carousel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="block">
+    <el-carousel :height="bannerHeight + 'px'">
+      <el-carousel-item v-for="(item,index) in carouselImg" :key="index">
+        <img :src="`../src/assets/carousel/${item.name}`" class="bannerImg" />
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bannerHeight: window.innerHeight,
+      carouselImg: [
+        {
+          name: '1.jpg',
+          id: 0
+        },
+        {
+          name: '2.jpg',
+          id: 0
+        },
+        {
+          name: '3.jpg',
+          id: 0
+        },
+        {
+          name: '4.jpg',
+          id: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    setSize: function () {
+      this.bannerHeight = 740 / 2560 * this.screenWidth
+      if(this.bannerHeight > 740) this.bannerHeight = 740
+      if(this.bannerHeight < 360) this.bannerHeight = 360
+    }
+  },
+  mounted() {
+    this.setSize();
+    const that = this;
+    window.addEventListener('resize', function() {
+      that.screenWidth = window.innerWidth;
+      that.setSize();
+    }, false);
+  }
+}
+</script>
+
+<style>
+.bannerImg {
+  width: 100%;
+  height: inherit;
+  object-fit: cover;
+  min-height: 360px;
+  min-width: 1400px;
+}
+</style>
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -4,10 +4,13 @@
         <div>
             <el-button @click="startHacking">Start</el-button>
         </div>
+        <Carousel />
     </div>
 </template>
 
 <script>
+import Carousel from './Carousel'
+
 export default {
     mounted() {
         console.log('加载成功')
@@ -15,12 +18,15 @@ export default {
     methods: {
         startHacking () {
             this.$notify({
-            title: 'It works!',
-            type: 'success',
-            message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-            duration: 5000
+                title: 'It works!',
+                type: 'success',
+                message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+                duration: 5000
             })
         }
+    },
+    components: {
+        Carousel
     }
 }
 </script>
